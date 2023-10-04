7a59bc1570eb814f47f951d90a99ad835fe34de4
Danilo99 <omotehinse99@gmail.com>
2023-10-04 11:15:01 +0100

    shopwith99-ecommerce

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">shopwith99</a>
+      </header>
+      <main>listed items.</main>
+    </div>
+  );
+}
+
+export default App;
